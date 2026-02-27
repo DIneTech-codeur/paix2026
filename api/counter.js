b429e1a4ebbfe31da3ec2ef75a9f0b4a84195c7f
DineTech <dinetechnet@gmail.com>
2026-02-27 17:37:29 +0100

    Update counter.js

--- api/counter.js
+++ api/counter.js
@@ -1,1 +1,16 @@
 
+let globalCount = 23847; // valeur initiale
+
+export default function handler(req, res) {
+
+  if (req.method === 'GET') {
+    return res.status(200).json({ count: globalCount });
+  }
+
+  if (req.method === 'POST') {
+    globalCount++;
+    return res.status(200).json({ count: globalCount });
+  }
+
+  return res.status(405).json({ message: 'Method not allowed' });
+}
